f5ee63f4240f8cadd48663b6654606b1129bbb26
Brwni <brianmm665@gmial.com>
2023-10-09 22:02:01 +0300

    Update

--- app/models.py
+++ app/models.py
@@ -10,19 +10,15 @@ class Hero(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String)
     super_name = db.Column(db.String)
-    created_at = db.Column(db.DateTime, server_default=db.func.now())
-    updated_at = db.Column(db.DateTime, onupdate=db.func.now())
-
     powers = db.relationship("Power", secondary="hero_powers", back_populates="heroes")
 
 
 class Power(db.Model):
     __tablename__ = "powers"
+    
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String)
     description = db.Column(db.String)
-    created_at = db.Column(db.DateTime, server_default=db.func.now())
-    updated_at = db.Column(db.DateTime, onupdate=db.func.now())
     heroes = db.relationship("Hero", secondary="hero_powers", back_populates="powers")
 
     @validates("description")
@@ -36,12 +32,11 @@ class Power(db.Model):
 
 class HeroPower(db.Model):
     __tablename__ = "hero_powers"
+    
     id = db.Column(db.Integer, primary_key=True)
     strength = db.Column(db.String)
     hero_id = db.Column(db.Integer, db.ForeignKey("heroes.id"))
     power_id = db.Column(db.Integer, db.ForeignKey("powers.id"))
-    created_at = db.Column(db.DateTime, server_default=db.func.now())
-    updated_at = db.Column(db.DateTime, onupdate=db.func.now())
 
     @validates("strength")
     def validate_strength(self, key, strength):
